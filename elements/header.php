08f8cfa4e1e80866744a79242daf8cfaeafab8cb
olivierlangelaar <olijf2002@gmail.com>
2014-02-24 18:11:57 +0100

    Added grid removed php-lint cache

--- elements/header.php
+++ elements/header.php
@@ -20,11 +20,11 @@ defined('C5_EXECUTE') or die(_("Access Denied."));
     <![endif]-->
   </head>
   <body>
-    <nav class="navbar navbar-default" role="navigation">
+    <nav class="navbar navbar-inverse" role="navigation">
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
